20b71986508650735385e2a9058c3975e930e9dd
tangxikang <tangxk@mail.sysu.edu.cn>
2025-08-13 23:53:14 +0800

    Update web.py

--- web.py
+++ web.py
@@ -22,7 +22,7 @@ st.set_page_config(
 st.markdown("""
 <style>
     html, body, [class*="css"]  {
-        font-size: 1rem; /* 全局基础字体，原本是16px，这里+4px */
+        font-size: 0.8rem; /* 全局基础字体，原本是16px，这里+4px */
     }
 </style>
 """, unsafe_allow_html=True)
@@ -174,6 +174,7 @@ if st.button("Start Prediction"):
     st.markdown("</div>", unsafe_allow_html=True)
 
     plt.close()
+
 
 
 
